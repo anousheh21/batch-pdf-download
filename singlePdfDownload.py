495867204e31e7edf405fd91a6706f18d7fcf4ad
anousheh21 <anousheh1@gmail.com>
2026-07-04 11:53:37 +0100

    Added functionality to download a single PDF to the desired location

---
+++ singlePdfDownload.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+import requests
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
+cookie = os.getenv("AUTH_COOKIE")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('downloadDestination')
+args = parser.parse_args()
+
+# Remove this eventually as it will come automated
+pdfUrl = 'https://learn.lboro.ac.uk/pluginfile.php/2137213/mod_resource/content/10/25COC102_W1Lec2-Tools.pdf'
+
+def getPdf(pdfUrl):
+    headers = {
+        "Cookie": cookie
+    }
+
+    response = requests.get(pdfUrl, headers=headers)
+
+    return response
+
+
+
+def checkUrlContentType(response):
+    if response.status_code != 200:
+        raise ValueError("None 200 Response: ", response.status_code)
+    
+    contentType = response.headers.get("Content-Type")
+    return contentType
+
+
+
+def checkValidDestination():
+    path = Path.home() / args.downloadDestination.lstrip("/")
+
+    if not path.is_dir(): 
+        raise NotADirectoryError("Invalid directory")
+    
+    return path
+
+
+
+def downloadPdf():
+    response = getPdf(pdfUrl)
+    contentType = checkUrlContentType(response)
+    downloadDir = checkValidDestination()
+
+    if not contentType.lower() or contentType.split(";", 1)[0].strip().lower() != "application/pdf":
+        raise TypeError("Incorrect Content Type")
+
+    # Get filename
+    content_disposition = response.headers.get("Content-Disposition")
+    filename = None
+    
+    if content_disposition:
+        filename = content_disposition.split("filename=")[-1].strip('""')
+    else:
+        filename = "download.pdf"
+
+    filePath = downloadDir / filename
+
+    counter = 1
+
+    while filePath.exists():
+        filePath = downloadDir / f"{Path(filename).stem} ({counter}){Path(filename).suffix}"
+        counter += 1
+
+    filePath.write_bytes(response.content)
+
+    print("File downloaded successfully!")
+
+
+
+if __name__ == "__main__":
+    downloadPdf()
+
